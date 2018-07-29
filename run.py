62013d90008af65eaf3ad05cb26bd2a4e008afc9
Tamir <tamir@chipchop.com.au>
2018-07-29 22:48:31 +1000

    trying to get a working menu and tests

--- run.py
+++ run.py
@@ -1,30 +1,96 @@
 import os
-from sys import exit
 
+
+context = {'location': 'outside the house',
+           'inventory': [],
+           'weapons': [],
+           'health': 100
+           }
+
+
+def get_help():
+    print("""the available commands are: 
+- map: shows you where you are on the map
+- enter (followed by a location name): lets you enter a location
+- take (followed by an item name): lets you take something into your possession
+- context: shows you everything you need to know about yourself
+- help: shows this menu""")
 
 os.system('clear')
-print("""Welcome to the game""")
-
-player_name = input('please enter your name: ')
-
-print(f'ok, {player_name}, let\'s play')
-print('the available commands are: map, enter (followed by a room name), take (followed by an item)')
+print('Welcome to the game')
+get_help()
 print('now go and save the world...')
 
-current_location = 'outside the house'
-action = input('> ')
+
+def get_context(context):
+    print('- you are', context['location'])
+    print('- you have', get_inventory(context))
+    print('- you have', get_weapons(context))
+    print('- your health is at', get_health_status(context), '%')
+
+
+def get_inventory(context):
+    if context['inventory']:
+        return context['inventory']
+    else:
+        return 'no inventory'
+
+
+def get_weapons(context):
+    if context['weapons']:
+        return context['weapons']
+    else:
+        return 'no weapons'
+
+
+def get_health_status(context):
+    return context['health']
 
 
 def map(current_location):
     print(f'you are currently standing {current_location}. there seems to be nothing around you apart from that')
 
 
-if action == 'map':
-    map(current_location)
-else:
-    exit(0)
 
-action = input('> ')
+while True:
+    action = input('> ')
+    if action == 'context':
+        get_context(context)
+    if action == 'map':
+        map(context['location'])
+    if action == 'help':
+        get_help()
 
-if action == 'enter house':
-    print('you have entered the house. there are a few rooms in the house.')
+
+
+#
+# class Room(object):
+#
+#     def enter(self, var):
+#         # var = 101
+#         print(f'{var}')
+#
+#
+#     def look(self):
+#         print('there not much to see here..')
+#
+#
+#     def exit(self):
+#         print('you have exited the room')
+#
+#
+#
+# class Monster(object):
+#
+#     def kill(self, use_weapon):
+#         if use_weapon:
+#             print('the beast is dead')
+#         else:
+#             print('you don\'t have a weapon. the beast killed you')
+#
+#
+# class Dragon(Monster):
+#
+#     def scream(self):
+#         print('i am a DRAGON! rarrrrrrr!')
+
---
+++ tests/tests.py
@@ -0,0 +1,6 @@
+from run import get_help
+
+
+def test_get_help():
+    get_help()
+    pass
